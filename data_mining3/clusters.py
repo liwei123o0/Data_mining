6a52da8acedfc9f8e2bb43548378a093cde46136
liwei <877129310@qq.com>
2015-04-01 17:53:23 +0800

    除去文本分析的语料库

---
+++ data_mining3/clusters.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#! /usr/bin/env python
+from Data_mining.data_mining2 import prxsim
+def readfile(filename):
+    lines =[line for line in file(filename)]
+
+    colnames=lines[0].strip().split('\t')[1:]
+    rownames=[]
+    data=[]
+    for line in lines[1:]:
+        p =line.strip().split('\t')
+        rownames.append(p[0])
+        data.append(float(x) for x in p[1:])
+    return rownames,colnames,data
---
+++ textanalysis/textfx.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+#! /usr/bin/env python
+from nltk.book import *
+print text4.dispersion_plot(["citizens","democracy","duties","America"])
